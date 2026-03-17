4edfa29c466e08928c8d89a8bc0fc6199960d223
staysa01-art <staysa01@gmail.com>
2026-03-16 22:50:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const axios = require('axios');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // Middleware
 app.use(cors());
 app.use(express.json());
